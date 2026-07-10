5c205d08ec8bb525852acb064cb7f3a85897152f
shifa-m <shifamahreen786@gmail.com>
2026-07-10 23:41:25 +0530

    constants

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { reducers } from './reducers';
+import App from './App';
+import './index.css';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>,
+  document.getElementById('root'),
+);
